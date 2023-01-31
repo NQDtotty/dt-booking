9409ff6da959f2af29b6e2909ecdb84d50a48dd8
NQDtotty <anhthoainguyen0907872008@gmail.com>
2023-01-31 15:17:36 +0700

    fix header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,10 +6,10 @@ export default function Header() {
     return (
         <header className='container-fluid'>
             <div className='row'>
-                <div className='col-md-2 logo'>
+                <div className='col-lg-3 logo'>
                     <h2><a href='#home'>DT-Booking</a></h2>
                 </div>
-                <div className='col-md-8 nav-menu'>
+                <div className='col-lg-6 nav-menu'>
                     <nav class="navbar navbar-expand-sm p-0">
                         <ul class="navbar-nav">
                             <li class="nav-item">
@@ -31,10 +31,10 @@ export default function Header() {
 
                     </nav>
                 </div>
-                <div className='col-md-2 nav-btn'>
+                <div className='col-lg-3 nav-btn'>
                     <nav class="navbar navbar-expand-sm p-0">
                         <ul class="navbar-nav">
-                            <li class="nav-item">
+                            <li class="nav-item log-in">
                                 <a class="nav-link" href="#">Đăng nhập</a>
                             </li>
                             <li class="nav-item sign-up">
