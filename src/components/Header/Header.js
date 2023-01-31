b81c2c1c1e4419d172b2514b3a23c6edd2a107de
NQDtotty <anhthoainguyen0907872008@gmail.com>
2023-01-31 09:47:03 +0700

    Initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Header.css';
+
+export default function Header() {
+    return (
+        <header className='container-fluid'>
+            <div className='row'>
+                <div className='col-md-2 logo'>
+                    <h2><a href='#home'>DT-Booking</a></h2>
+                </div>
+                <div className='col-md-8 nav-menu'>
+                    <nav class="navbar navbar-expand-sm p-0">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#home">Trang chủ</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Lịch trình</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Liên hệ</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Hóa đơn</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Về chúng tôi</a>
+                            </li>
+                        </ul>
+
+                    </nav>
+                </div>
+                <div className='col-md-2 nav-btn'>
+                    <nav class="navbar navbar-expand-sm p-0">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Đăng nhập</a>
+                            </li>
+                            <li class="nav-item sign-up">
+                                <a class="nav-link" href="#">Đăng kí</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    )
+}
